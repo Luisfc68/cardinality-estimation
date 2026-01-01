029914473e17d1a6ad554d788c5098f13d6c3456
Luisfc68 <luisfc68@gmail.com>
2026-01-01 23:32:11 +0100

    optimize recordinality

--- recordinality.py
+++ recordinality.py
@@ -3,12 +3,14 @@ import randomhash
 from collections.abc import Iterable
 from typing import Any
 from common import RunResult
+from heapq import heappush, heappop
 
 def create_recordinality(k: int):
     MAX_HASH = 2**32 - 1
     hash_family = randomhash.RandomHashFamily(count=1)  # recordinality only uses one hash function
     def recordinality_instance(stream: Iterable[Any]):
         records = set()
+        heap = []
         stream_size = 0
         number_of_records = 0
         for element in stream:
@@ -17,13 +19,16 @@ def create_recordinality(k: int):
             element_not_in_records = element_hash not in records
             if len(records) < k and element_not_in_records:
                 records.add(element_hash)
+                heappush(heap, element_hash)
                 number_of_records += 1
                 continue
 
-            min_record = min(records)
+            min_record = heap[0]
             if element_not_in_records and element_hash > min_record:
-                records.remove(min_record)
+                removed = heappop(heap)
+                records.remove(removed)
                 records.add(element_hash)
+                heappush(heap, element_hash)
                 number_of_records += 1
         estimation = (k * (1 + 1/k)**(number_of_records - k + 1)) - 1
         return RunResult(
